db6fa7817b2e7658dda2d8fc2e4ae0b6da12b875
JAYANTPARIHAR-CODER <JAYANTPARIHAR170@GMAIL.COM>
2024-09-26 16:27:01 +0530

    c

---
+++ Frontend/html/a = 0.py
@@ -0,0 +1,18 @@
+# a = 0 
+# while a<= 10:
+#     print("hello")
+#     a = a +5
+
+
+
+num = 10
+sum = 0
+while num>=1:
+    sum=sum+num
+    num=num-1
+    print(sum)
+
+num =int(input("Enter a number"))
+
+    
+
